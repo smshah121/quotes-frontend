52e6fb6826d65c95ec7f40f6a4b95e3e812a1bd8
smshah121 <smshah.2003@gmail.com>
2026-04-12 14:11:36 +0500

    cs

--- src/features/user/userApi.js
+++ src/features/user/userApi.js
@@ -5,7 +5,7 @@ export const userApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: import.meta.env.VITE_API_URL,
         prepareHeaders:(headers)=> {
-            const token = localStorage.getItem("token")
+            const token = localStorage.getItem("access_token")
             if(token){
                 headers.set("Authorization", `Bearer ${token}`)
             }
