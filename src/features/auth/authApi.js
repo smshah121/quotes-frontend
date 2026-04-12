b1cb6a7087df8b8da8b566309ec81fa5729a45dd
smshah121 <smshah.2003@gmail.com>
2026-04-12 11:56:13 +0500

    cs

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -3,7 +3,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL, // your NestJS API base URL
+    baseUrl: import.meta.env.VITE_API_URL,
+    prepareHeaders: (headers)=> {
+            const token = localStorage.getItem("token")
+            if(token){
+                headers.set("Authorization", `Bearer ${token}`)
+            }
+
+            return headers
+        }
   }),
   endpoints: (builder) => ({
     login: builder.mutation({
